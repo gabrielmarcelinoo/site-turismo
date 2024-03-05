87951d208bd507ae0c5fc0398440920f90a313e3
gabrielmarcelinoo <gabriel.marcelino.1302@gmail.com>
2024-03-05 18:23:24 -0300

    adicionando parte dos pontos turisticos

--- maps/script.js
+++ maps/script.js
@@ -10,7 +10,7 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -22.849482, lng: -45.230091 },
         zoom: 13,
-        styles: [/* Estilos do mapa */]
+        styles: []
     });
     
 
